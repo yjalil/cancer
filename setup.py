f73d05ff79359833e14d64e367c195422b2e4afe
abdeljalil yahya <jalil.yahy@gmail.com>
2024-03-01 19:58:12 +0100

    gitignores

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(name='cancer',
+      description="No description",
+      packages=find_packages(),
+      install_requires=requirements)
